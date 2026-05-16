594c042c59b8f61ac961f8f339136f377b2f515f
Bassam-Elsaied <93853900+Bassam-Elsaied@users.noreply.github.com>
2026-05-16 14:14:01 +0300

    add Google analytics.  Google tag manager

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,8 +1,11 @@
- import type { Metadata } from "next";
+import type { Metadata } from "next";
+import { Suspense } from "react";
 import "./globals.css";
 import { NextIntlClientProvider } from "next-intl";
 import RenderInProvider from "@/components/Global/RenderInProvider";
 import ProgressBar from "@/components/Global/ProgressBar";
+import GoogleAnalytics from "@/components/Global/GoogleAnalytics";
+import GoogleTagManager from "@/components/Global/GoogleTagManager";
 import "react-phone-number-input/style.css";
 import "react-datepicker/dist/react-datepicker.css";
 import { ToastContainer } from "react-toastify";
@@ -51,6 +54,10 @@ export default async function RootLayout({
         <script dangerouslySetInnerHTML={{ __html: themeScript }} />
       </head>
       <body suppressHydrationWarning>
+        <Suspense fallback={null}>
+          <GoogleTagManager />
+          <GoogleAnalytics />
+        </Suspense>
         <ProgressBar />
         <ToastContainer
           position={locale === "ar" ? "top-left" : "top-right"}
